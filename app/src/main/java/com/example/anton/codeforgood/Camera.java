1219da20ad78ddff3bc18d7f798ec1a695bede41
relisher <anton.relin@gmail.com>
2015-03-28 14:01:45 -0400

    commit camera calling crap

--- app/src/main/java/com/example/anton/codeforgood/Camera.java
+++ app/src/main/java/com/example/anton/codeforgood/Camera.java
@@ -1,31 +1,24 @@
 package com.example.anton.codeforgood;
 
-import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 
-public class VendorMainActivity extends ActionBarActivity {
+public class Camera extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_vendor_main);
+        setContentView(R.layout.activity_camera);
     }
 
-    public void addDonation(View view) {
-        Intent intent = new Intent(this, Camera.class);
-        startActivity(intent);
-
-    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_vendor_main, menu);
+        getMenuInflater().inflate(R.menu.menu_camera, menu);
         return true;
     }
 
