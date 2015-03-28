f9ccf2248b50c6856f203e4a7bdaccdf18133437
relisher <anton.relin@gmail.com>
2015-03-28 13:05:48 -0400

    add login

--- app/src/main/java/com/example/anton/codeforgood/MainActivity.java
+++ app/src/main/java/com/example/anton/codeforgood/MainActivity.java
@@ -1,39 +1,52 @@
 package com.example.anton.codeforgood;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+public class MainActivity extends Activity {
 
-public class MainActivity extends ActionBarActivity {
-
+    private EditText  username=null;
+    private EditText  password=null;
+    private TextView attempts;
+    private Button login;
+    int counter = 3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        username = (EditText)findViewById(R.id.editText1);
+        password = (EditText)findViewById(R.id.editText2);
+        attempts = (TextView)findViewById(R.id.textView5);
+        attempts.setText(Integer.toString(counter));
+        login = (Button)findViewById(R.id.button1);
     }
 
+    public void login(View view){
+        if(username.getText().toString().equals("admin") &&
+                password.getText().toString().equals("admin")){
+            Toast.makeText(getApplicationContext(), "Redirecting...",
+                    Toast.LENGTH_SHORT).show();
+        }
+        else{
+            Toast.makeText(getApplicationContext(), "Wrong Credentials",
+                    Toast.LENGTH_SHORT).show();
+            attempts.setBackgroundColor(Color.RED);
+            counter--;
+            attempts.setText(Integer.toString(counter));
+            if(counter==0){
+                login.setEnabled(false);
+            }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
         }
 
-        return super.onOptionsItemSelected(item);
     }
+
+
 }
